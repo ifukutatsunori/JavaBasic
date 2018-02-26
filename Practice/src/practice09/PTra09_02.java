72f33e422d7b17e60ab50fe2f59824f92844bfe7
DESKTOP-ECR12M6\likeit_student <tatsunori0712@gmail.com>
2018-02-26 18:48:13 +0900

    10-3

--- Practice/src/practice09/PTra09_02.java
+++ Practice/src/practice09/PTra09_02.java
@@ -12,11 +12,13 @@ public class PTra09_02 {
 
 		// ★ 配列arrayを、Util内のメソッドを用いてランダムに並び替えてください
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
-		Util.shuffleArray(array);
+		String[] shuffledArray = Util. shuffleArray(array);
+
 
 
 		// ★ ランダムに並び替えた後の配列を前から順に全て表示してください
-		 System.out.println();
-
+		for(String place : shuffledArray) {
+			System.out.println(place);
+		}
 	}
 }
---
+++ Practice/src/practice10/Car.java
@@ -0,0 +1,16 @@
+package practice10;
+
+import java.util.Random;
+
+public class Car {
+	int seriaNo;
+	String color;
+	int gasoline;
+
+	int run() {
+
+		this.gasoline=rnd.nextInt(15);
+	return
+
+	}
+}
